635c3d32402823cffe084e427a04aed0fc093b89
Daniel Román <danielroman.05111@gmail.com>
2024-04-16 21:18:46 -0500

    feat(Main) ValidationPipes

--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,8 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-  
-  const port = process.env.PORT
+
+  const port = process.env.PORT;
   await app.listen(port, () => {
     console.log(`App on port ${port}`);
   });
