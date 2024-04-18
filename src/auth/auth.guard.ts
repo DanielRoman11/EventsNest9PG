bcb2b277bbd41b3f64a66de08384697f6f5aef72
Daniel Román <danielroman.05111@gmail.com>
2024-04-18 02:03:54 -0500

    feat(Auth): Auth guard, localStrategy,

--- src/auth/auth.guard.ts
+++ src/auth/auth.guard.ts
@@ -19,14 +19,9 @@ export class AuthGuard implements CanActivate {
       throw new UnauthorizedException();
     }
     try {
-      const payload = await this.jwtService.verifyAsync(
-        token,
-        {
-          secret: constants.jwtSecret
-        }
-      );
-      // 💡 We're assigning the payload to the request object here
-      // so that we can access it in our route handlers
+      const payload = await this.jwtService.verifyAsync(token, {
+        secret: constants.jwtSecret,
+      });
       request['user'] = payload;
     } catch {
       throw new UnauthorizedException();
