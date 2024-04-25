10a86ce2396d93365f53ab5c23aca7fab6b34616
Daniel Román <danielroman.05111@gmail.com>
2024-04-25 00:11:18 -0500

    fix(Auth): SignUp on route /auth/signup

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -13,15 +13,26 @@ import {
 import { AuthService } from './auth.service';
 import { JwtAuthGuard } from './jwt-auth.guard';
 import { User } from 'src/user/entities/user.entity';
+import { UserService } from 'src/user/users.service';
+import { CreateUserDto } from 'src/user/dto/create-user.dto';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private authService: AuthService) {}
+  constructor(
+    private authService: AuthService,
+    private readonly userService: UserService,
+  ) {}
 
   @HttpCode(HttpStatus.OK)
   @Post('login')
   async signIn(@Body() signInDto: Record<string, any>) {
     return await this.authService.signIn(signInDto.email, signInDto.password);
+  }
+
+  @UseInterceptors(ClassSerializerInterceptor)
+  @Post('signup')
+  async signUp(@Body() input: CreateUserDto) {
+    return await this.userService.signUp(input);
   }
 
   @UseGuards(JwtAuthGuard)
