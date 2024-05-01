1d47a1a9de0bf8b79bbced73ac1493260a963100
Daniel Román <danielroman.05111@gmail.com>
2024-04-30 22:30:31 -0500

    feat(envFile): Routing env file on global config

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,19 @@ import { EventModule } from './event/event.module';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { AppController } from './app.controller';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [DatabaseModule, EventModule, AuthModule, UserModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `.env.${process.env.NODE_ENV}`,
+    }),
+    DatabaseModule,
+    EventModule,
+    AuthModule,
+    UserModule,
+  ],
   controllers: [AppController],
 })
 export class AppModule {}
